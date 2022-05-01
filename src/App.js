2b8364696be9b0847aafbc02e99af1ac9358ef1f
chess2022 <lakeshore.hnh@gmail.com>
2022-04-30 17:26:59 -0700

    Adds spread operator shortcut

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ import WeatherForecast from "./components/WeatherForecast"
 export default function App() {
   // console.log(weatherData);
   const allWeatherData = weatherData.map((ele, index) => {
-    return  <WeatherForecast img={ele.img} conditions={ele.conditions} time={ele.time} key={index}/>
+    return (
+      <WeatherForecast
+        // img={ele.img}
+        // conditions={ele.conditions}
+        // time={ele.time}
+        {...ele} //spread operator to "destructure" and add all these - need to have same name as your data for this to work!
+        key={index}
+      />
+    );
   })
   return (
     <div className="App">
